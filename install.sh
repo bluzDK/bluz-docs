a915d2563c9fdf1f2085ee10df1aa15041704c74
Eric Ely <eric.j.ely@gmail.com>
2018-03-31 09:24:55 -0400

    need aws cli

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ source env/bin/activate
 sudo apt-get update
 sudo apt-get install -y python-pip
 sudo apt-get install -y python-dev
+sudo apt-get -y -qq install awscli
 pip install jinja2==2.8
 pip install mkdocs
